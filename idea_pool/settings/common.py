3d62786f3746fab489668530e2c6a023595f8548
Protosac <protosacq@gmail.com>
2018-08-13 20:23:31 -0700

    Update drf settings.

--- idea_pool/settings/common.py
+++ idea_pool/settings/common.py
@@ -7,7 +7,7 @@ https://docs.djangoproject.com/en/dev/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/dev/ref/settings/
 """
-import os
+import os, datetime
 from os.path import dirname
 
 # Build paths inside the project like this: os.path.join(SETTINGS_DIR, ...)
@@ -51,15 +51,15 @@ DJANGO_APPS = [
 
     # Blog
     'accounts',
-    # 'blog',
-    # 'core',
+    'ideas',
 ]
 
 EXTERNAL_APPS = [
     # Tools
     'rest_framework',
     'django_extensions',
-    'rest_framework_extensions'
+    'rest_framework_extensions',
+    'rest_framework_jwt',
 ]
 INSTALLED_APPS = DJANGO_APPS + EXTERNAL_APPS
 
@@ -170,8 +170,43 @@ AUTH_PASSWORD_VALIDATORS = [
 # The credentials below are NOT FOR PRODUCTION. Overwrite them by editing
 # your dev.py
 # =======================================================================
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10,
+}
 
+JWT_AUTH = {
+    'JWT_SECRET_KEY': os.getenv('DJANGO_SECRET_KEY'),
+    'JWT_ALLOW_REFRESH': True,
+    'JWT_AUTH_HEADER_PREFIX': 'JWT',
 
+    'JWT_ENCODE_HANDLER': 'rest_framework_jwt.utils.jwt_encode_handler',
+    'JWT_DECODE_HANDLER': 'rest_framework_jwt.utils.jwt_decode_handler',
+    'JWT_PAYLOAD_HANDLER': 'rest_framework_jwt.utils.jwt_payload_handler',
+    'JWT_PAYLOAD_GET_USER_ID_HANDLER': 'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
+
+    'JWT_RESPONSE_PAYLOAD_HANDLER': 'rest_framework_jwt.utils.jwt_response_payload_handler',
+    'JWT_GET_USER_SECRET_KEY': None,
+    'JWT_PUBLIC_KEY': None,
+    'JWT_PRIVATE_KEY': None,
+    'JWT_ALGORITHM': 'HS256',
+    'JWT_VERIFY': True,
+    'JWT_VERIFY_EXPIRATION': True,
+    'JWT_LEEWAY': 0,
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=600),
+    'JWT_AUDIENCE': None,
+    'JWT_ISSUER': None,
+    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
+    'JWT_AUTH_COOKIE': None,
+}
 # =======================================================================
 # LOGGING SETTINGS
 # https://docs.djangoproject.com/en/1.11/topics/logging
--- idea_pool/settings/dev.py
+++ idea_pool/settings/dev.py
@@ -85,6 +85,39 @@ AUTH_PASSWORD_VALIDATORS += [
 ]
 
 # =======================================================================
+# THIRD-PARTY INTEGRATION SETTINGS
+# For settings specific to third-party modules and apis.
+# The credentials below are NOT FOR PRODUCTION. Overwrite them by editing
+# your dev.py
+# =======================================================================
+
+JWT_AUTH = {
+    'JWT_SECRET_KEY': SECRET_KEY,
+    'JWT_ALLOW_REFRESH': True,
+    'JWT_AUTH_HEADER_PREFIX': 'JWT',
+
+    'JWT_ENCODE_HANDLER': 'rest_framework_jwt.utils.jwt_encode_handler',
+    'JWT_DECODE_HANDLER': 'rest_framework_jwt.utils.jwt_decode_handler',
+    'JWT_PAYLOAD_HANDLER': 'rest_framework_jwt.utils.jwt_payload_handler',
+    'JWT_PAYLOAD_GET_USER_ID_HANDLER':
+        'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
+
+    'JWT_RESPONSE_PAYLOAD_HANDLER': 'rest_framework_jwt.utils.jwt_response_payload_handler',
+    'JWT_GET_USER_SECRET_KEY': None,
+    'JWT_PUBLIC_KEY': None,
+    'JWT_PRIVATE_KEY': None,
+    'JWT_ALGORITHM': 'HS256',
+    'JWT_VERIFY': True,
+    'JWT_VERIFY_EXPIRATION': True,
+    'JWT_LEEWAY': 0,
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=300),
+    'JWT_AUDIENCE': None,
+    'JWT_ISSUER': None,
+    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
+    'JWT_AUTH_COOKIE': None,
+}
+
+# =======================================================================
 # LOGGING SETTINGS
 # =======================================================================
 LOGGING = {
